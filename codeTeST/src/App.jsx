e62b115e8149052deda48df6d33eccf32ae06d75
Amiel Brencis Salipande <absalipande@gmail.com>
2023-01-27 11:31:36 +0800

    fix: refactored code

--- codeTeST/src/App.jsx
+++ codeTeST/src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import Users from './components/Users';
 
-function App() {
-  return <Users />;
-}
+const App = () => {
+  const [contributors, setContributors] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.github.com/repos/microsoft/typescript/contributors?per_page=10'
+        );
+        setContributors(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // error handling
+  if (error) {
+    return <div className='notification is-danger'>An error occured: {error.message}</div>;
+  }
+
+  return (
+    <table className='table is-bordered mt-5'>
+      <thead>
+        <tr>
+          <th>Profile Picture</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contributors.map((contributor) => (
+          <tr key={contributor.id}>
+            <td>
+              <img src={contributor.avatar_url} alt={contributor.login} />
+            </td>
+            <td>{contributor.login}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default App;
